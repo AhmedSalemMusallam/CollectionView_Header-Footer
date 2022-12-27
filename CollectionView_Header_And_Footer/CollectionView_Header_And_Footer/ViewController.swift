c185dd95f29979b3c270eb00391c63b65f04b063
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2022-12-28 00:13:10 +0200

    The Collection View Header and Footer Project Creation Commit - Initial Commit

---
+++ CollectionView_Header_And_Footer/CollectionView_Header_And_Footer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView_Header_And_Footer
+//
+//  Created by Ahmed Salem on 28/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
